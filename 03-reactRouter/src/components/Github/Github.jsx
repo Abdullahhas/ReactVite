41dd55629d4c4612321a7f520bd061ac2332b83e
abdullahhas <abdullah312003@gmail.com>
2024-10-19 00:31:49 +0500

    react router and project

---
+++ 03-reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,22 @@
+import { data } from "@remix-run/router";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/abdullahhas")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="" width={300} />
+    </div>
+  );
+}
+
+export default Github;

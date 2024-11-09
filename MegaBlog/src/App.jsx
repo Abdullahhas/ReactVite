d3443ca761b8b27998ce34e8b960a02b9a7cd7e3
abdullahhas <abdullah312003@gmail.com>
2024-11-09 20:51:17 +0500

    mega project

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { useEffect } from 'react'
+import { login , logout } from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+     authService.getCurrentUser()
+     .then((userData)=>{
+      if(userData)
+      {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+     })
+     .finally(()=> setLoading(false))
+  } , [])
 
   return (
     <>

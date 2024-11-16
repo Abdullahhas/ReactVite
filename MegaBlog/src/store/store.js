d905c73dbac77d85e84e8c384303d81cccd55b88
abdullahhas <abdullah312003@gmail.com>
2024-11-16 15:58:13 +0500

    adding input agin  etc

--- MegaBlog/src/store/store.js
+++ MegaBlog/src/store/store.js
@@ -1,9 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit'
+// import {configureStore} from '@reduxjs/toolkit'
+// import authSlice from './authSlice';
+// const store = configureStore({
+//     reducer:{
+//         auth : authSlice,
+//     }
+// })
+
+// export default store
+
+import { configureStore } from "@reduxjs/toolkit";
 import authSlice from './authSlice';
 const store = configureStore({
     reducer:{
         auth : authSlice,
     }
 })
-
 export default store

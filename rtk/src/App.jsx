e2e82544f46688a6dabbaade84bb4f5a1382bfc4
abdullahhas <abdullah312003@gmail.com>
2025-03-22 18:14:58 +0500

    redux

---
+++ rtk/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div className="bg-red-200" > 
+    hello
+   </div>
+   </>
+  )
+}
+
+export default App
